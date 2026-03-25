cb1ce047fb11ee316f60181f8ea3ad29d117b81b
Hakusai-Butyou <hakusaibutyou@gmail.com>
2026-03-26 03:07:58 +0900

    add - セッションへの登録関数作成

--- src/server/session.rs
+++ src/server/session.rs
@@ -1,2 +1,3 @@
 pub mod generate_session_id;
+pub mod register_session;
 pub mod init_redis;
---
+++ src/server/session/register_session.rs
@@ -0,0 +1,24 @@
+use leptos::prelude::*;
+use leptos::server_fn::ServerFnError;
+use std::sync::Arc;
+use crate::server::session::generate_session_id::generate_session_id;
+use surrealdb_types::{RecordId, ToSql};
+#[cfg(feature = "ssr")]
+use redis::{Client, AsyncTypedCommands };
+
+#[cfg(feature = "ssr")]
+pub async fn register_session(user_record_id:RecordId)->Result<String,ServerFnError>{
+    if let Some(client) = use_context::<Arc<Client>>(){
+        let session_id=generate_session_id();
+        let record_id=user_record_id.to_sql();
+        let mut conection=client.get_multiplexed_async_connection().await?;
+        let _=conection.set(session_id.clone(), record_id).await;
+        Ok(session_id)
+    } else {
+        Err(ServerFnError::new("Redis not found."))
+    }
+}
+#[cfg(not(feature = "ssr"))]
+pub async fn register_session(_:RecordId)->Result<String,ServerFnError>{
+    Err(ServerFnError::new("csr mode."))
+}

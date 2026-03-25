9fc1fd764683cd1ecec74b2b3be9f2afdea38720
Hakusai-Butyou <hakusaibutyou@gmail.com>
2026-03-26 03:05:11 +0900

    add | change - googleのIDトークンのデコード後データに対応する構造体作成とデコード関数修正

--- src/server/auth/google.rs
+++ src/server/auth/google.rs
@@ -3,10 +3,33 @@ use leptos::server_fn::{ServerFnError,codec};
 use jsonwebtoken::{decode, DecodingKey, Validation, Algorithm};
 use base64::Engine;
 use base64::engine::general_purpose::URL_SAFE_NO_PAD;
-//use serde_json::Value;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Header{
+    pub alg:String,
+    pub kid:String,
+    pub typ:String,
+}
+#[derive(Deserialize, Debug)]
+pub struct Payload{
+    pub iss:String,
+    pub azp:String,
+    pub aud:String,
+    pub sub:String,
+    pub email:String,
+    pub email_verified:bool,
+    pub nbf:i64,
+    pub name:String,
+    pub picture:String,
+    pub given_name:String,
+    pub iat:i64,
+    pub exp:i64,
+    pub jti:String
+}
 
 /// Google の ID Token の payload を JSON に変換する（署名検証なし）
-pub fn decode_google_id_token(token: &str) -> (Vec<u8>,Vec<u8>) {
+pub fn decode_google_id_token(token: &str) -> (Header,Payload) {
     let parts: Vec<&str> = token.split('.').collect();
     let header_b64 = parts[0];
     let payload_b64 = parts[1];
@@ -17,8 +40,9 @@ pub fn decode_google_id_token(token: &str) -> (Vec<u8>,Vec<u8>) {
     let decoded_payload = URL_SAFE_NO_PAD
         .decode(payload_b64)
         .expect("Base64 decode failed");
-    (decoded_header,decoded_payload)
-    //serde_json::from_slice(&decoded).expect("JSON parse failed")
+    let header:Header=serde_json::from_slice(&decoded_header).unwrap();
+    let payload:Payload=serde_json::from_slice(&decoded_payload).unwrap();
+    (header,payload)
 }
 #[derive(serde::Deserialize,serde::Serialize,Debug,Clone)]
 pub struct GoogleLoginInput {
